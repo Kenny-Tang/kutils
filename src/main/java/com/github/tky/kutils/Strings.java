2e33bc81a29e60f1a35e4584727036ac19d0bac0
kenny-tang <kenny-tang@hotmail.com>
2021-01-13 10:47:47 +0800

    code format

--- src/main/java/com/github/tky/kutils/Strings.java
+++ src/main/java/com/github/tky/kutils/Strings.java
@@ -9,83 +9,89 @@ import com.google.common.base.CaseFormat;
  *
  */
 public class Strings extends org.apache.commons.lang3.StringUtils {
-    
-    /**
-     * 将字符串调整成可以在 in 条件中使用的形式<br/>
-     *  <pre>
-     *      eg: a,b,c 将转化成 'a','b','c'
-     *  </pre>
-     * @param inList
-     * @return string can append to a sql
-     */
-    public static String splitAsInCondation(String inList) {
-        String[] list = inList.split(",");
-        StringBuffer condation = new StringBuffer(" ");
-        for (int i = 0; i < list.length; i++) {
-            if (!isBlank(list[i]))
-                condation.append("'").append(list[i].trim()).append("',");
-        }
-        condation.replace(condation.length() - 1, condation.length(), " ");
-        return condation.toString();
-    }
-    
-    /**
-     * 
-     * @param <T>
-     * @param inList
-     * @param handler
-     * @return string can append to a sql
-     * 	
-     */
-    public static  <T> String splitAsInCondation(java.util.List<T> inList, InPropertyHandler<T> handler) {
-        StringBuffer condation = new StringBuffer("");
-        for (int i = 0; i < inList.size(); i++) {
-            String inItem = null ;
-            if(handler != null) {
-                inItem = handler.inItem(inList.get(i)) ;
-            }else {
-                inItem = inList.get(i).toString() ;
-            }
-            if (!isBlank(inItem))
-                condation.append("'").append(inItem.trim()).append("',");
-        }
-        condation.replace(condation.length() - 1, condation.length(), "");
-        return condation.toString();
-    }
-    
-    /**
-     * 将大写开始的驼峰命名转换为小写开始的驼峰命名字符串
-     * @param string
-     * @return 小写开始的驼峰命名字符串
-     */
-    public static String uppperCamelToLowerCamel(String string) {
-        return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, string);
-    }
-    
-    /**
-     * 将小写开始的驼峰命名转换为大写开始的驼峰命名
-     * @param string
-     * @return 大写开始的驼峰命名字符串
-     */
-    public static String lowerCamelToUpperCamel(String string) {
-        return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, string);
-    }
-    
-    /**
-     * 将下划线转换为小写开始驼峰命名
-     * @param string
-     * @return 小写开始的驼峰命名字符串
-     */
-    public static String lowerUnderscoreToLowerCamel(String string) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
-    }
-    
-    /**
-     * 将下划线转换为大写开始驼峰命名
-     * @param string
-     * @return 大写开始的驼峰命名字符串
-     */
-    public static String lowerUnderscoreToUpperCamel(String string) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, string);
-    }
+
+	/**
+	 * 将字符串调整成可以在 in 条件中使用的形式<br/>
+	 * 
+	 * <pre>
+	 *      eg: a,b,c 将转化成 'a','b','c'
+	 * </pre>
+	 * 
+	 * @param inList
+	 * @return string can append to a sql
+	 */
+	public static String splitAsInCondation(String inList) {
+		String[] list = inList.split(",");
+		StringBuffer condation = new StringBuffer(" ");
+		for (int i = 0; i < list.length; i++) {
+			if (!isBlank(list[i]))
+				condation.append("'").append(list[i].trim()).append("',");
+		}
+		condation.replace(condation.length() - 1, condation.length(), " ");
+		return condation.toString();
+	}
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param inList
+	 * @param handler
+	 * @return string can append to a sql
+	 * 
+	 */
+	public static <T> String splitAsInCondation(java.util.List<T> inList, InPropertyHandler<T> handler) {
+		StringBuffer condation = new StringBuffer("");
+		for (int i = 0; i < inList.size(); i++) {
+			String inItem = null;
+			if (handler != null) {
+				inItem = handler.inItem(inList.get(i));
+			} else {
+				inItem = inList.get(i).toString();
+			}
+			if (!isBlank(inItem))
+				condation.append("'").append(inItem.trim()).append("',");
+		}
+		condation.replace(condation.length() - 1, condation.length(), "");
+		return condation.toString();
+	}
+
+	/**
+	 * 将大写开始的驼峰命名转换为小写开始的驼峰命名字符串
+	 * 
+	 * @param string
+	 * @return 小写开始的驼峰命名字符串
+	 */
+	public static String uppperCamelToLowerCamel(String string) {
+		return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, string);
+	}
+
+	/**
+	 * 将小写开始的驼峰命名转换为大写开始的驼峰命名
+	 * 
+	 * @param string
+	 * @return 大写开始的驼峰命名字符串
+	 */
+	public static String lowerCamelToUpperCamel(String string) {
+		return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, string);
+	}
+
+	/**
+	 * 将下划线转换为小写开始驼峰命名
+	 * 
+	 * @param string
+	 * @return 小写开始的驼峰命名字符串
+	 */
+	public static String lowerUnderscoreToLowerCamel(String string) {
+		return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
+	}
+
+	/**
+	 * 将下划线转换为大写开始驼峰命名
+	 * 
+	 * @param string
+	 * @return 大写开始的驼峰命名字符串
+	 */
+	public static String lowerUnderscoreToUpperCamel(String string) {
+		return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, string);
+	}
 }
