c150a0eb2ec4e087005beaba4907cfe06733233c
paopaofish <994987301@qq.com>
2021-01-26 12:40:15 +0800

    添加date的parseDate的测试

--- src/test/java/com/github/tky/kutils/DatesTest.java
+++ src/test/java/com/github/tky/kutils/DatesTest.java
@@ -1,13 +1,40 @@
 package com.github.tky.kutils;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.test.BaseTest;
 
 public class DatesTest extends BaseTest {
+
+    @Test
+    public void testParseDate() {
+        String str = "2020-01-25";
+        String pat = "yyyy-MM-dd";
+        String[] pats = new String[]{"yyyy-MM-dd"};
+
+        Date tmp = Dates.parseDate(str,pat );
+        Date tmp1 = null;
+        try {
+            tmp1 = DateUtils.parseDate(str,pat);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date target = null;
+        try {
+            target = DateUtils.parseDate( str,pats );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(target);
+        Assert.assertEquals( tmp,target );
+        Assert.assertEquals( tmp,tmp1 );
+    }
 
     @Test
     public void testFirstDayOfNextMonth() {
